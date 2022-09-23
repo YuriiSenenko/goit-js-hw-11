f8f8f1e4106fe7413948820f5e656cfd9aceb0e4
YuriiSenenko <yurkosenenko@gmail.com>
2022-09-23 15:00:19 +0300

    Finish update

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
+
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '30036034-49bdb558087010c436563671a';
 const perPage = 40;
 
 export default async function fetchImages(searchValue, numberPage) {
-  const response = await fetch(
-    `${URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${numberPage}`
-  );
-  return await response.json();
+  try {
+    const response = await axios.get(
+      `${URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${numberPage}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
