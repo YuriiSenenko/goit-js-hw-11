17ed64bdda29a276d5bfdd63564b5b216b6d3cc6
YuriiSenenko <yurkosenenko@gmail.com>
2022-09-23 13:30:16 +0300

    Update fetchImages

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -2,10 +2,9 @@ const URL = 'https://pixabay.com/api/';
 const API_KEY = '30036034-49bdb558087010c436563671a';
 const perPage = 40;
 
-export default function fetchImages(searchValue, numberPage) {
-  return fetch(
+export default async function fetchImages(searchValue, numberPage) {
+  const response = await fetch(
     `${URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${numberPage}`
-  ).then(response => {
-    return response.json();
-  });
+  );
+  return await response.json();
 }
